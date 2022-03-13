bdb82d744a49646494f07148e693829c81bbd72e
Lorenzobattistela <lorenzowottrich@gmail.com>
2022-03-13 20:24:28 -0300

    adding route for workouts

--- healthier/src/components/navbar.jsx
+++ healthier/src/components/navbar.jsx
@@ -2,6 +2,7 @@ import { Navbar } from 'react-bootstrap';
 import React from 'react';
 import logo from '../assets/logo.png';
 import '../styles/navbar.css';
+import { Link } from "react-router-dom";
 
 class NavigationBar extends React.Component{
     render(){
@@ -10,14 +11,14 @@ class NavigationBar extends React.Component{
                 <Navbar.Brand href="/">
                     <img src={ logo } alt='logo' className='nav-logo'/>
                 </Navbar.Brand>
-                <Navbar.Brand href="/"><p className='nav-link' >Treinos</p></Navbar.Brand>
-                <Navbar.Brand href="/"><p className='nav-link' >Alimentação</p></Navbar.Brand>
-                <Navbar.Brand href="/"><p className='nav-link' >Perfil</p></Navbar.Brand>
+                <Navbar.Brand href="/"><Link to='/treinos'><p className='nav-link' >Treinos</p></Link></Navbar.Brand>
+                <Navbar.Brand href="/"><Link to='/alimentacao'><p className='nav-link' >Alimentação</p></Link></Navbar.Brand>
+                <Navbar.Brand href="/"><Link to='/perfil'><p className='nav-link' >Perfil</p></Link></Navbar.Brand>
                 <button className='login-button' >
-                    <Navbar.Brand  href="/"><p className='nav-link' >Login</p></Navbar.Brand>
+                    <Navbar.Brand  href="/"><Link to='/login'><p className='nav-link' >Login</p></Link></Navbar.Brand>
                 </button>
                 <button className='create-button' >
-                    <Navbar.Brand  href="/"><p className='nav-link' >Criar Conta</p></Navbar.Brand>
+                    <Navbar.Brand  href="/"><Link to='/criar-conta' ><p className='nav-link' >Criar Conta</p></Link></Navbar.Brand>
                 </button>
             </Navbar>
         )
--- healthier/src/index.js
+++ healthier/src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import Treinos from './routes/treinos.jsx';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="treinos" element={<Treinos />} />
+      </Routes>
+  </BrowserRouter>,
   </React.StrictMode>,
   document.getElementById('root')
 );
